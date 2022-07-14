6dfb9fc2a2e1dcfa2f0ed0f98e9dacfda2963b86
matannagar <73398186+matannagar@users.noreply.github.com>
2022-07-14 15:49:32 +0300

    added buttons

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 const TodosList = ({ todos, setTodos }) => {
     return (
-        <div>Todos List</div>
+        <div>
+            {
+                todos.map((todo) => (
+                    <li className="list-item" key={todo.id}>
+                        <input
+                            type="text"
+                            value={todo.title}
+                            className="list"
+                            onChange={(event) => event.preventDefault()} />
+                        <div>
+                            {/* complete task */}
+                            <button className="button-complete task-button">
+                                <i className="fa fa-check-circle"></i>
+                            </button>
+                            {/* edit task */}
+                            <button className="button-edit task-button">
+                                <i className="fa fa-edit"></i>
+                            </button>
+                            {/* delete task */}
+                            <button className="button-delete task-button">
+                                <i className="fa fa-trash"></i>
+                            </button>
+                        </div>
+                    </li>
+                ))
+            }
+        </div>
     )
 }
 
